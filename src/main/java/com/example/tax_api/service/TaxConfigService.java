64a5e73f5ac83466c8e1cc0f264309b1dbb291f5
Swati Kasina <Swatikasina21@gmail.com>
2025-10-08 13:57:53 -0400

    Initial Commit

---
+++ src/main/java/com/example/tax_api/service/TaxConfigService.java
@@ -0,0 +1,40 @@
+package com.example.tax_api.service;
+
+import com.example.tax_api.model.TaxConfig;
+import com.example.tax_api.repository.TaxConfigRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaxConfigService {
+
+    private final TaxConfigRepository repository;
+
+    public TaxConfigService(TaxConfigRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<TaxConfig> getAllConfigs() {
+        return repository.findAll();
+    }
+
+    public List<TaxConfig> getListFromQuery() {
+        return repository.getListFromQuery();
+    }
+
+    public TaxConfig createConfig(TaxConfig config) {
+        return repository.save(config);
+    }
+
+    public TaxConfig getConfigById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("TaxConfig not found with id: " + id));
+    }
+
+    public void deleteConfig(Long id) {
+        repository.deleteById(id);
+    }
+}
+
+
