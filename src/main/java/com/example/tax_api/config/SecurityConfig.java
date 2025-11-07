93a772811c0d86a888ccf55a8209a90a41fcb10c
Swati Kasina <Swatikasina21@gmail.com>
2025-11-07 01:42:50 -0500

    Cahnges

--- src/main/java/com/example/tax_api/config/SecurityConfig.java
+++ src/main/java/com/example/tax_api/config/SecurityConfig.java
@@ -9,18 +9,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain security(HttpSecurity http) throws Exception {
-        return http
-                .cors(Customizer.withDefaults()) // enable CORS support
-                .csrf(csrf -> csrf.disable()) // disable if you're stateless API
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() // preflight
-                        .requestMatchers(HttpMethod.GET, "/api/tax/config").permitAll()
-                        .requestMatchers("/actuator/health", "/actuator/health/**", "/actuator/info", "/actuator",
-                                "/actuator/**")
-                        .permitAll() // allow actuator endpoints for health checks
-                        .anyRequest().authenticated())
-                .build();
-    }
+        @Bean
+        public SecurityFilterChain security(HttpSecurity http) throws Exception {
+                return http
+                                .cors(Customizer.withDefaults()) // enable CORS support
+                                .csrf(csrf -> csrf.disable()) // disable if you're stateless API
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() // preflight
+                                                .requestMatchers(HttpMethod.GET, "/api/tax/config").permitAll()
+                                                .requestMatchers("/actuator/health", "/actuator/health/**",
+                                                                "/actuator/info", "/actuator",
+                                                                "/actuator/**")
+                                                .permitAll() // allow actuator endpoints for health checks
+                                                .anyRequest().authenticated())
+                                .build();
+        }
 }
