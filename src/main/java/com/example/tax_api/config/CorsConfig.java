7fd44ac17c4fa32376cf00fcbee05cc3f14bdc53
Swati Kasina <Swatikasina21@gmail.com>
2025-11-07 19:37:48 -0500

    CHange

---
+++ src/main/java/com/example/tax_api/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.tax_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://tax-frontend.s3-website-us-east-1.amazonaws.com",
+                                "https://tax-frontend.s3-website-us-east-1.amazonaws.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+
+        };
+    }
+}
