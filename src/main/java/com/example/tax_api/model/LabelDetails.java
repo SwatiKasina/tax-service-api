8ce3edbc0cbb86b3af5798bb5059237e6b8629ca
Swati Kasina <Swatikasina21@gmail.com>
2026-01-06 15:58:11 -0500

    Labels branch single commit

---
+++ src/main/java/com/example/tax_api/model/LabelDetails.java
@@ -0,0 +1,22 @@
+package com.example.tax_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+
+    public class LabelDetails {
+
+        private String key;
+        private String value;
+
+
+
+    }
