9dcd41f634e4970caa37028adc877dd5542d87e6
SwatiKasina <157078567+SwatiKasina@users.noreply.github.com>
2026-01-20 17:07:10 -0500

    Changes in inventory files (#34)
    Co-authored-by: Swati Kasina <Swatikasina21@gmail.com>

--- src/main/java/com/example/tax_api/controller/InventoryController.java
+++ src/main/java/com/example/tax_api/controller/InventoryController.java
@@ -1,30 +1,31 @@
 package com.example.tax_api.controller;
 
 import com.example.tax_api.model.Inventory;
+import com.example.tax_api.model.PosSaleRequest;
 import com.example.tax_api.service.InventoryService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 
 @RequiredArgsConstructor
-@RequestMapping("/inventory")
+@RequestMapping("/api")
 
 
 public class InventoryController {
 
-    private  final InventoryService inventoryService;
+    private final InventoryService inventoryService;
 
     @GetMapping("/{skuNumber}")
- public ResponseEntity<?> getInventoryById(@PathVariable String skuNumber){
+    public ResponseEntity<?> getInventoryById(@PathVariable String skuNumber) {
         Inventory inventory = inventoryService.getBySkuNumber(skuNumber);
         if (inventory != null) {
             return ResponseEntity.ok(inventory);
@@ -33,4 +34,38 @@ public class InventoryController {
             return ResponseEntity.status(404).body(error);
         }
     }
+
+    @GetMapping("/inventory")
+    public ResponseEntity<List<Inventory>> getAllProducts() {
+        List<Inventory> inv = inventoryService.getAllInventory();
+        return ResponseEntity.ok(inv);
+    }
+
+    @PostMapping
+            (
+                    value = "/row", consumes = MediaType.APPLICATION_JSON_VALUE,
+                    produces = MediaType.APPLICATION_JSON_VALUE
+            )
+    public ResponseEntity<?> addnewRow(@RequestBody Inventory inventory) {
+        try {
+            Inventory saved = inventoryService.createRow(inventory);
+            return ResponseEntity.ok(saved);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+        @PutMapping("/{skuNumber}")
+                public ResponseEntity <Inventory> put (@PathVariable String skuNumber, @RequestBody Inventory i ){
+                   Inventory update = inventoryService.putMethod(skuNumber, i );
+                   return ResponseEntity.ok(update);
+        }
+
+        @DeleteMapping("/{skuNumber}")
+    public ResponseEntity <?> delete (@PathVariable String skuNumber){
+      inventoryService.deleteMethod(skuNumber);
+      return ResponseEntity.noContent().build();
+        }
+
+
 }
