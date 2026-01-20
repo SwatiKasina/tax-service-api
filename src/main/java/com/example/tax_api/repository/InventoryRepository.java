9fb27ed930b9ba4b8cda27e084b681c5278a0901
Swati Kasina <Swatikasina21@gmail.com>
2026-01-20 17:02:11 -0500

    Changes in inventory files

--- src/main/java/com/example/tax_api/repository/InventoryRepository.java
+++ src/main/java/com/example/tax_api/repository/InventoryRepository.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, String> {
 
-    @Query(value = "SELECT * FROM dolly.inventory", nativeQuery = true)
-    List<Inventory> getListFromQuery();
+
 }
 
--- src/main/java/com/example/tax_api/service/InventoryService.java
+++ src/main/java/com/example/tax_api/service/InventoryService.java
@@ -4,6 +4,8 @@ import com.example.tax_api.model.Inventory;
 import com.example.tax_api.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
@@ -12,10 +14,45 @@ public class InventoryService {
         this.inventoryRepository = inventoryRepository;
     }
 
-    public Inventory getBySkuNumber (String skuNumber){
+    public Inventory getBySkuNumber(String skuNumber) {
         return inventoryRepository.findById(skuNumber)
                 .orElseThrow(() ->
                         new RuntimeException("Invalid Sku Number"));
 
     }
+
+    public List<Inventory> getAllInventory() {
+        return inventoryRepository.findAll();
+    }
+
+    //Create a row
+    // Create a row
+    public Inventory createRow(Inventory item) {
+        try {
+            return inventoryRepository.save(item);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Sku Number Exists");
+        }
+
+    }
+
+    public Inventory putMethod(String skuNumber, Inventory i) {
+        inventoryRepository.findById(skuNumber)
+                .orElseThrow(() ->
+                        new RuntimeException("error"));
+
+        return inventoryRepository.save(i);
+    }
+
+
+    //Delete
+    public void deleteMethod(String skuNumber) {
+        inventoryRepository.findById(skuNumber)
+                        .orElseThrow(() -> new
+                                RuntimeException("Sku Not Found"));
+        inventoryRepository.deleteById(skuNumber);
+    }
+
+
 }
